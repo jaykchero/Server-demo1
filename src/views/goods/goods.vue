c713e52846423944fd5895cd05f6520674781540
huangle <jay3le@qq.com>
2020-08-09 11:46:41 +0800

    测试2

---
+++ src/views/goods/goods.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2>商品页面</h2>
+       <ul>
+       	<li v-for="(item,index) in list " :key="index">
+         
+         商品名： {{item.name}} <br>
+         价格： {{item.price}} <br>
+         数量：  {{item.num}} <br>
+        <button @click="add(item)">添加至购物车</button><br>
+        </li>
+       </ul>
+
+  </div>
+</template>
+
+<script>
+  export default {
+      data () {
+
+        return {
+
+        }
+      },
+      methods:{
+        add(item){
+           this.$store.commit('addShopping',item)
+        }
+
+      },
+      computed:{
+        list () {
+           return this.$store.state.list;
+        }
+
+      }
+
+  }
+</script>
+
+<style>
+</style>
---
+++ src/views/goods/shoppingCat.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>购物车页面</h2>
+    <ul>
+    	<li v-for="(item,index) in list" :key="index">
+        商品名:{{item.name}}
+        价格:{{item.price}}
+        数量:{{item.num}}
+        单品总计: {{item.price*item.num}}
+      </li>
+    </ul>
+
+   <div>
+      <h2>总计：{{total}}</h2>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    date () {
+      return {
+        sum:''
+      }
+    },
+    computed:{
+      list () {
+        return this.$store.state.addList;
+      },
+      total () {
+        return this.$store.getters.total
+      }
+
+
+    },
+    created() {
+
+    }
+
+
+
+
+  }
+</script>
+
+<style>
+</style>
